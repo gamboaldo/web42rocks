da0934cf124898b0fddd708b6286ccfe4b78c484
Aldo Mata <gamboaldo@gmail.com>
2021-06-03 10:58:49 -0700

    commit

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const server = express();
 require("dotenv").config();
+const cors = require("cors");
+const path = require("path");
+
+server.use(express.json());
+server.use(cors());
+server.use(express.static(path.join(__dirname, "client/build")));
 
 const PORT = process.env.PORT || 5000;
+
+server.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 server.get("/api", (req, res) => {
   res.json({ message: "i am cool" });
