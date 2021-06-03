50397a6e3e543605d945306c5be7a3d3c20d61b1
Aldo Mata <gamboaldo@gmail.com>
2021-06-03 11:07:46 -0700

    k

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
-const express = require("express");
-const server = express();
 require("dotenv").config();
+const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
+const server = express();
+
 server.use(express.json());
 server.use(cors());
-server.use(express.static(path.join(__dirname, "client/build")));
+server.use(express.static(path.join(__dirname, "client/build"))); // static assets
+
+console.log(process.env.USER); // env USER=gabriel
+console.log(process.env.SHELL); // env SHELL=/bin/zhs
+
+if (process.env.NODE_ENV === "production") {
+  console.log("this means this code is deployed");
+}
 
 const PORT = process.env.PORT || 5000;
 
+console.log("port is -> ", PORT);
+
 server.get("/", (req, res) => {
+  // sending back index.html
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 server.get("/api", (req, res) => {
-  res.json({ message: "i am cool" });
+  res.json({ message: ` ROCKS` });
 });
 
 server.use((req, res) => {
-  res.status(404).json({ message: "cant find anything" });
+  res.status(404).json({ message: "not found sorry" });
 });
+
 server.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
 });
