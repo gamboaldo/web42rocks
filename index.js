9cb4154b09cc95b77305697dceb25c10fa2ab9c8
Aldo Mata <gamboaldo@gmail.com>
2021-06-03 10:17:21 -0700

    wip

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello Aldo");
+const express = require("express");
+const server = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+server.get("/api", (req, res) => {
+  res.json({ message: "i am cool" });
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
