6289bc1a578223b6b393702ff72965660c143992
Aldo Mata <gamboaldo@gmail.com>
2021-06-03 10:31:41 -0700

    eip

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ server.get("/api", (req, res) => {
   res.json({ message: "i am cool" });
 });
 
+server.use((req, res) => {
+  res.status(404).json({ message: "cant find anything" });
+});
 server.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
 });
